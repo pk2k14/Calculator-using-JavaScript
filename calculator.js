69719de56413ce7f0613b673b68a1294a3b83e6a
pk2k14 <32770209+pk2k14@users.noreply.github.com>
2018-09-13 19:16:18 +0530

    HTML,CSS and JS files uploaded

---
+++ calculator.js
@@ -0,0 +1,79 @@
+function one() 
+		{
+			var x = document.getElementById("screen").value;
+    			document.getElementById("screen").value = x+"1";
+		}
+		function two() 
+		{
+			var x = document.getElementById("screen").value;
+    			document.getElementById("screen").value = x+"2";
+		}
+		function three() 
+		{
+			var x = document.getElementById("screen").value;
+    			document.getElementById("screen").value = x+"3";
+		}
+		function minus() 
+		{
+			var x = document.getElementById("screen").value;
+    			document.getElementById("screen").value = x+"-";
+		}
+
+		function four() 
+		{
+			var x = document.getElementById("screen").value;
+    			document.getElementById("screen").value = x+"4";
+		}
+		function five() 
+		{
+			var x = document.getElementById("screen").value;
+    			document.getElementById("screen").value = x+"5";
+		}
+		function six() 
+		{
+			var x = document.getElementById("screen").value;
+    			document.getElementById("screen").value = x+"6";
+		}
+		function plus() 
+		{
+			var x = document.getElementById("screen").value;
+    			document.getElementById("screen").value = x+"+";
+		}
+
+		function seven() 
+		{
+			var x = document.getElementById("screen").value;
+    			document.getElementById("screen").value = x+"7";
+		}
+		function eight() 
+		{
+			var x = document.getElementById("screen").value;
+    			document.getElementById("screen").value = x+"8";
+		}
+		function nine() 
+		{
+			var x = document.getElementById("screen").value;
+    			document.getElementById("screen").value = x+"9";
+		}
+	
+		function zero() 
+		{
+			var x = document.getElementById("screen").value;
+    			document.getElementById("screen").value = x+"0";
+		}
+		function equal() 
+		{
+			var x = document.getElementById("screen").value;
+    			document.getElementById("screen").value = eval(x);
+		}
+		function multiply() 
+		{
+			var x = document.getElementById("screen").value;
+    			document.getElementById("screen").value = x+"*";
+		}
+		function divide() 
+		{
+			var x = document.getElementById("screen").value;
+    			document.getElementById("screen").value = x+"/";
+		}
+				
